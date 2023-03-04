115916b01ac49908cafba498e88bd1e7402d9912
Artem <artembariga5@gmail.com>
2023-03-04 22:48:58 +0200

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timeId = null;
+
+btnStartEl.addEventListener('click', startChangeBodyBg);
+btnStopEl.addEventListener('click', stopChangeBodyBg);
+
+function startChangeBodyBg () {
+    btnStartEl.disabled = 'disabled';
+
+    timeId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopChangeBodyBg () {
+    btnStartEl.removeAttribute('disabled');
+    clearInterval(timeId);
+}
